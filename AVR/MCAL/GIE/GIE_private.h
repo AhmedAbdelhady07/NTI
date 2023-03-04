0b9519ecb0baf53828aef840d59f5486cddb03f8
AhmedAbdelhady07 <126583618+AhmedAbdelhady07@users.noreply.github.com>
2023-03-04 21:46:31 +0200

    Add files via upload

---
+++ AVR/MCAL/GIE/GIE_private.h
@@ -0,0 +1,15 @@
+/*
+ * GIE_pvt.h
+ *
+ *  Created on: Feb 7, 2023
+ *      Author: DELL
+ */
+
+#ifndef GIE_GIE_PVT_H_
+#define GIE_GIE_PVT_H_
+
+/*************  SREG Register  *************/
+#define	SREG_REG		(*((volatile u8 *)0x5F))
+
+#define I_BIT			7
+#endif /* GIE_GIE_PVT_H_ */
