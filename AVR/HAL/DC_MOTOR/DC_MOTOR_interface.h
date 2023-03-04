8c67d463058ea78ca865be61986a80bfcc415c7b
AhmedAbdelhady07 <126583618+AhmedAbdelhady07@users.noreply.github.com>
2023-03-04 21:57:58 +0200

    Create DC_MOTOR_interface.h

---
+++ AVR/HAL/DC_MOTOR/DC_MOTOR_interface.h
@@ -0,0 +1,14 @@
+/*******************************************************************/
+/*************** Name      : Ahmed Abdelhady   **********************/
+/*************** Date      : 23/2/2023  	      **********************/
+/*************** SWC       : DC_MOTOR  **********************/
+/*************** File Name : MOTOR_interface.h     **********************/
+/*************** Version   : 1.0        	  **********************/
+/*******************************************************************/
+#ifndef HAL_DC_MOTOR_MOTOR_INT_H_
+#define HAL_DC_MOTOR_MOTOR_INT_H_
+
+void H_MOTOR_void_init	(void);
+void H_MOTOR_void_run	(u8 Copy_u8Duty);
+
+#endif /* HAL_DC_MOTOR_MOTOR_INT_H_ */
