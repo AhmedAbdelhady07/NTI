ad5d8491606d27596b6d23d06b89ea753dc709e4
AhmedAbdelhady07 <126583618+AhmedAbdelhady07@users.noreply.github.com>
2023-03-04 21:45:37 +0200

    Create GIE_interface.h

---
+++ AVR/MCAL/GIE/GIE_interface.h
@@ -0,0 +1,18 @@
+/*
+ * GIE_int.h
+ *
+ *  Created on: Feb 7, 2023
+ *      Author: Ahmed Abdelhady
+ */
+
+#ifndef GIE_GIE_INT_H_
+#define GIE_GIE_INT_H_
+
+/****************      Public Macros     ****************/
+
+
+/***************   Function Prototypes   ****************/
+void M_GIE_void_enable		(void);
+void M_GIE_void_disable		(void);
+
+#endif /* GIE_GIE_INT_H_ */
